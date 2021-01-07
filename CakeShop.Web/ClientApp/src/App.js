d85aea9ff9c76bf275e79f935ccc317c46603948
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-01-07 22:48:22 +0200

    Implement display add products user is logged in

--- CakeShop.Web/ClientApp/src/App.js
+++ CakeShop.Web/ClientApp/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
@@ -6,17 +6,17 @@ import { Home } from './components/Home';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
 
-import './custom.css'
+import './custom.css';
 import AddProduct from './components/Products/AddProduct';
 
 export default class App extends Component {
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-            <Route exact path='/addProduct' component={ AddProduct }/>
-        <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route path='/addProduct' component={AddProduct} />
+                <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+            </Layout>
+        );
+    }
 }
--- CakeShop.Web/ClientApp/src/components/NavMenu.js
+++ CakeShop.Web/ClientApp/src/components/NavMenu.js
@@ -1,54 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LoginMenu } from './api-authorization/LoginMenu';
 import './NavMenu.css';
 
+import authorizeService from './api-authorization/AuthorizeService';
+
 export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+    constructor(props) {
+        super(props);
 
-  constructor (props) {
-    super(props);
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true,
+            isLoggedIn: false,
+        };
+    }
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+    componentDidMount() {
+        authorizeService.isAuthenticated().then(r => {
+            this.setState({
+                isLoggedIn: r,
+            })
+        })
+    }
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
 
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">CakeShop</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                 <NavItem>
-                    <NavLink tag={ Link } className="text-dark" to="/about">About</NavLink>
-                 </NavItem>
-                 <NavItem>
-                     <NavLink tag={ Link } className="text-dark" to="/products">Products</NavLink>
-                 </NavItem>
-                 <NavItem>
-                     <NavLink tag={ Link } className="text-dark" to="/addProduct">Add Product</NavLink>
-                 </NavItem>
-                <LoginMenu></LoginMenu>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
+
+
+    render() {
+        return (
+            <header>
+                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+                    <Container>
+                        <NavbarBrand tag={Link} to="/">CakeShop</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                            <ul className="navbar-nav flex-grow">
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/about">About</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/products">Products</NavLink>
+                                </NavItem>
+                                {this.state.isLoggedIn ? (
+                                    <NavItem>
+                                        <NavLink tag={Link} className="text-dark" to="/addProduct">Add Product</NavLink>
+                                    </NavItem>
+                                ) : (
+                                        <Fragment></Fragment>
+                                    )}
+                                <LoginMenu></LoginMenu>
+                            </ul>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </header>
+        );
+    }
 }
